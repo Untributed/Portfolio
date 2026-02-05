44ba6dd95b6934e84d185b04435bdf9cabee1cce
Yoannis Getov <untributed@gmail.com>
2026-02-05 22:28:32 +0200

    Added Libra's Essence source code
    -Added Navigation search bar
    -Added Navigation Links (No links currently)
    -Added Backgrounds

---
+++ Libra's Essence/src/App.jsx
@@ -0,0 +1,35 @@
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./components/Home.jsx";
+import NewArrivals from "./components/NewArrivals.jsx";
+import "./styles/App.css";
+
+export default function App() {
+
+  return (
+    <>
+      <div className="searchbox-container">
+        <img className="logo" src="./src/assets/logo.jpg" alt="Logo" />
+
+        <div className="input-wrapper">
+          <input className="searchbox" type="text" placeholder='Search' />
+          <span className="material-symbols-outlined search-icon">search</span>
+        </div>
+      </div>
+
+      <div className="nav-container">
+        <ul className="nav-list">
+          <li>New Arrivals</li>
+          <li>Promotions</li>
+          <li>Perfumes</li>
+          <li>Manufacturers</li>
+          <li>Sets</li>
+        </ul>
+      </div>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="new-arrivals" element={<NewArrivals />} />
+      </Routes>
+    </>
+  )
+}
