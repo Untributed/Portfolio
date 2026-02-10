c36abac1aad3d8f2eb681df5c2ae8e0821964b78
Yoannis Getov <untributed@gmail.com>
2026-02-10 22:34:16 +0200

    Minor Fix
    -Fixed moving icons on hover: Facebook and x moved sometimes slightly on the left or right

--- Libra's Essence/src/components/Footer.jsx
+++ Libra's Essence/src/components/Footer.jsx
@@ -5,9 +5,9 @@ export default function Footer() {
         <>
             <div className="social-icons-wrapper">
                 <ul className="social-icons">
-                    <li><a className="facebook-icon" href="https://www.facebook.com/" target="_blank"> <FaFacebookF /> </a></li>
-                    <li><a className="x-icon" href="https://x.com/home" target="_blank"> <FaXTwitter /> </a></li>
-                    <li><a className="instagram-icon" href="https://www.instagram.com/" target="_blank"> <FaInstagram /> </a></li>
+                    <li><a className="facebook-icon" href="https://www.facebook.com/" target="_blank"> <FaFacebookF /></a></li>
+                    <li><a className="x-icon" href="https://x.com/home" target="_blank"> <FaXTwitter /></a></li>
+                    <li><a className="instagram-icon" href="https://www.instagram.com/" target="_blank"> <FaInstagram /></a></li>
                     <li><a className="youtube-icon" href="https://www.youtube.com/" target="_blank"> <FaYoutube /></a></li>
                 </ul>
             </div>
