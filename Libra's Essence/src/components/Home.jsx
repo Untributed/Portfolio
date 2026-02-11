2036dc04512eb55078989dfeddc24672ff796532
Yoannis Getov <untributed@gmail.com>
2026-02-11 23:40:17 +0200

    Update version 0.3
    -Added Product pictures inside of public folder
    -Added Static product catalog cards
    -Added Static Product name inside of main products wrapper
    -Imported static fa star from library fa6 and changed color to yellowish
    -Added static rating
    -Added static price
    -Added Product buy button (div)
    -Added stylization for products catalog

--- Libra's Essence/src/components/Home.jsx
+++ Libra's Essence/src/components/Home.jsx
@@ -1,11 +1,41 @@
 import Header from "../components/Header.jsx";
 import Footer from "../components/Footer.jsx";
+import { FaStar } from "react-icons/fa6";
 
 export default function Home() {
 
     return (
         <>
-           <p>Home Page</p>
+            <main className="products-catalog-wrapper">
+                <div className="product-card-wrapper">
+                    <div className="product-card">
+                        <ul className="products-ul">
+                            <li className="azure-li">
+                                <div className="azure-fragrance-img">
+                                    <img className="azure-fragrance" src="./public/azure.png" alt="Product" />
+                                </div>
+                                <div className="azure-name-div">
+                                    <a href="product">Azure Whisper</a>
+                                </div>
+                                <div className="rating">
+                                <a className="star-icon"> <FaStar /></a>
+                                <a className="star-icon"> <FaStar /></a>
+                                <a className="star-icon"> <FaStar /></a>
+                                <a className="star-icon"> <FaStar /></a>
+                                <a className="star-icon"> <FaStar /></a>
+                                <span className="rating"> (5.0)</span>
+                                </div>
+                                <span className="price-span">
+                                    19$
+                                </span>
+                                <div className="buy-button">
+                                    Buy
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </main>
         </>
     );
 }
