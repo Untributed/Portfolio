e04d38e08f1748e9068d66b7e5c0b7e60d1abc6e
Yoannis Getov <untributed@gmail.com>
2026-02-25 00:38:13 +0200

    Minor CSS Changes
    -Changed Footer logic so it get's pushed down when new content is added.
    -Added overflow prevention for the future content + tested to see if content overflows or pushes the footer to the bottom adding scrollbar.
    Changed ul links animation behavior, removed ease all and from 4 seconds to 2.
    
    Removed:
    -Removed static product template.

--- Libra's Essence/src/components/Home.jsx
+++ Libra's Essence/src/components/Home.jsx
@@ -6,37 +6,7 @@ export default function Home() {
 
     return (
         <div className="products-catalog-wrapper">
-                <div className="product-card-wrapper">
-                    <div className="product-card">
-                        <ul className="products-ul">
-                            <li className="azure-li">
-                                <div className="azure-fragrance-img">
-                                    <img className="azure-fragrance" src="./src/assets/azure.png" alt="Product" />
-                                </div>
-                                <div className="azure-name-div">
-                                    <a href="product">Azure Whisper</a>
-                                </div>
-                                <div className="rating">
-                                    <a className="star-icon"> <FaStar /></a>
-                                    <a className="star-icon"> <FaStar /></a>
-                                    <a className="star-icon"> <FaStar /></a>
-                                    <a className="star-icon"> <FaStar /></a>
-                                    <a className="star-icon"> <FaStar /></a>
-                                    <span className="rating"> (5.0)</span>
-                                </div>
-                                <span className="price-span">
-                                    19$
-                                </span>
-                                <div className="buy-button">
-                                    Buy
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
 
-
-
-                </div>
         </div>
     );
 }
