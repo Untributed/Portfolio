248f435372683efd12556a9ca449b6d9cb01578c
Yoannis Getov <untributed@gmail.com>
2026-02-28 11:11:20 +0200

    CSS ul-list update
    -Fixed animations for the ul-list links
    -Added working styles *functional* lines underneath the links

--- Libra's Essence/src/components/Header.jsx
+++ Libra's Essence/src/components/Header.jsx
@@ -3,29 +3,29 @@ export default function Header() {
     return (
         <div className="header-wrapper">
             <div className="header-sub-wrapper">
-                    <div className="searchbox-container">
-                        <Link to="/"><img className="logo" src="./src/assets/logo.jpg" alt="Logo" /></Link>
-                        <div className="input-wrapper">
-                            <input className="searchbox" id="searchbox" type="text" placeholder='Search' />
-                            <span className="material-symbols-outlined search-icon">search</span>
-                        </div>
-                        <div className="header-icons">
-                            <span className="material-symbols-outlined icon-person">person</span>
-                            <span className="material-symbols-outlined shopping_cart">shopping_cart</span>
-                        </div>
+                <div className="searchbox-container">
+                    <Link to="/"><img className="logo" src="./src/assets/logo.jpg" alt="Logo" /></Link>
+                    <div className="input-wrapper">
+                        <input className="searchbox" id="searchbox" type="text" placeholder='Search' />
+                        <span className="material-symbols-outlined search-icon">search</span>
                     </div>
+                    <div className="header-icons">
+                        <span className="material-symbols-outlined icon-person">person</span>
+                        <span className="material-symbols-outlined shopping_cart">shopping_cart</span>
+                    </div>
+                </div>
 
 
-                </div>
-                <div className="nav-links-wrapper">
-                    <ul className="nav-ul-links">
-                        <Link className="new-arrivals" to="new-arrivals"><li>New Arrivals</li></Link>
-                        <Link className="promotions" to="promotions"><li>Promotions</li></Link>
-                        <Link className="perfumes" to="perfumes"><li>Perfumes</li></Link>
-                        <Link className="manufacturers" to="manufacturers"><li>Manufacturers</li></Link>
-                        <Link className="sets" to="sets"><li>Sets</li></Link>
-                    </ul>
-                </div>
             </div>
+            <div className="nav-links-wrapper">
+                <ul className="nav-ul-links">
+                    <div className="new-arrivals"><Link to="new-arrivals"><li>New Arrivals</li></Link></div>
+                    <div className="promotions"><Link to="promotions"><li>Promotions</li></Link></div>
+                    <div className="perfumes"><Link to="perfumes"><li>Perfumes</li></Link></div>
+                    <div className="manufacturers"><Link to="manufacturers"><li>Manufacturers</li></Link></div>
+                    <div className="sets"><Link to="sets"><li>Sets</li></Link></div>
+                </ul>
+            </div>
+        </div>
     );
 }
