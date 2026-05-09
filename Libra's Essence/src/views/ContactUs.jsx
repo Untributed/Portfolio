909caf0a447235c67c947bb23eb2bc5503575206
Yoannis Getov <untributed@gmail.com>
2026-05-09 10:47:26 +0300

    Update
    -Updated ContactUs.jsx and added more stylization to position across x and y axis, seperated the content a little bit and adjusted the font size
    -Added Unfinished grid layout for the Contact form (Work in progress).
    -Adjusted and fixed youtube fa icon below in the footer.jsx component

--- Libra's Essence/src/views/ContactUs.jsx
+++ Libra's Essence/src/views/ContactUs.jsx
@@ -1,19 +1,33 @@
 export default function ContactUs() {
     return (
         <div className="contact-us-wrapper">
-            <h1>Contact Us:</h1>
-            <p>For all inquiries</p>
-            <div className="phone-span">
-                <span class="material-symbols-outlined">phone_in_talk</span>
-                <span className="about-us-span">Phone: (415) 555-0287;</span>
+            <div className="contacts-wrapper">
+                <h1>Contact Us:</h1>
+                <p>We’d love to hear from you! Whether you have questions, feedback, or need support, feel free to get in touch with us.</p>
+                <p className="call-info">
+                    *Calls are charged at standard operator rates for BTC 0700 landline numbers.</p>
+                <div className="phone-span">
+                    <span class="material-symbols-outlined">phone_in_talk</span>
+                    <span className="about-us-span">Phone: (415) 555-0287;</span>
+                </div>
+                <div className="email-span">
+                    <span className="material-symbols-outlined">mail</span>
+                    <span className="about-us-span">Email: info@lefragrance.com</span>
+                </div>
+                <span className="about-us-span">Address: 742 Willow Crest Avenue
+                    <br /> Texas, 78702 United States</span>
+                <p>Working Hours: Monday-Friday: 10:00 to 18:00</p>
             </div>
-            <div className="email-span">
-                <span className="material-symbols-outlined">mail</span>
-                <span className="about-us-span">Email: info@libras_essence.com</span>
+            <div className="contact-form-wrapper">
+                <div className="contact-form">
+                    <input className="box-1" type="text" />
+                    <input className="box-2" type="text" />
+                    <input className="box-3" type="text" />
+                    <input className="box-4" type="text" />
+                    <input className="box-5" type="text" />
+                    <input className="box-6" type="text" />
+                </div>
             </div>
-            <span className="about-us-span">Address: 742 Willow Crest Avenue
-                <br /> Texas, 78702 United States</span>
-            <p>Working Hours: Monday-Friday: 10:00 to 18:00</p>
         </div>
     )
 }
