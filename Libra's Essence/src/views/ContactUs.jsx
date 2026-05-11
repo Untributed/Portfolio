d1526d4618e66758f9ce1043d041a8e0daf32a45
Yoannis Getov <untributed@gmail.com>
2026-05-11 19:56:06 +0300

    Update Version 0.6 (Major Update)
    -Added Submit form with inputs (not yet working).
    -Added Submit button.
    -Added Submit button form and button css.
    -Added Textarea and textare css.
    -Added display flex to submit form.
    
    -Removed:
    -Removed Display grid from Submit form.
    -Removed some unnecessary styles and adjusted some text.

--- Libra's Essence/src/views/ContactUs.jsx
+++ Libra's Essence/src/views/ContactUs.jsx
@@ -1,13 +1,13 @@
 export default function ContactUs() {
     return (
-        <div className="contact-us-wrapper">
-            <div className="contacts-wrapper">
-                <h1>Contact Us:</h1>
+        <div className="contact-page-wrapper">
+            <div className="contact-us-info-wrapper">
+                <h1>Contacts:</h1>
                 <p>We’d love to hear from you! Whether you have questions, feedback, or need support, feel free to get in touch with us.</p>
                 <p className="call-info">
                     *Calls are charged at standard operator rates for BTC 0700 landline numbers.</p>
                 <div className="phone-span">
-                    <span class="material-symbols-outlined">phone_in_talk</span>
+                    <span className="material-symbols-outlined">phone_in_talk</span>
                     <span className="about-us-span">Phone: (415) 555-0287;</span>
                 </div>
                 <div className="email-span">
@@ -19,14 +19,18 @@ export default function ContactUs() {
                 <p>Working Hours: Monday-Friday: 10:00 to 18:00</p>
             </div>
             <div className="contact-form-wrapper">
+                <h1>Submit a form:</h1>
                 <div className="contact-form">
-                    <input className="box-1" type="text" />
-                    <input className="box-2" type="text" />
-                    <input className="box-3" type="text" />
-                    <input className="box-4" type="text" />
-                    <input className="box-5" type="text" />
-                    <input className="box-6" type="text" />
+                    <div className="box-group-row">
+                    <input className="box-1" type="text" placeholder="Name" required />
+                    <input className="box-2" type="text" placeholder="Last Name" required />
+                    <input className="box-3" type="email" placeholder="Email" required />
+                    <input className="box-4" type="phone" placeholder="Phone" required />
+                    </div>
+                    <input className="box-5" type="text" placeholder="Topic" required />
+                    <textarea className="box-6" type="text" required />
                 </div>
+                <button className="submit-btn" type="submit">SUBMIT</button>
             </div>
         </div>
     )
