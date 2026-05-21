26189d546c5afa02bc8ed05a2cd3090c64797cc3
Yoannis Getov <untributed@gmail.com>
2026-05-21 12:48:13 +0300

    Update 0.7 completed (About us section)
    -Pushed to Production
    -Added more sections with information such as h1,h2,p and information
    -Added css to look semetric.

--- Libra's Essence/src/views/AboutUs.jsx
+++ Libra's Essence/src/views/AboutUs.jsx
@@ -13,12 +13,40 @@ export default function AboutUs() {
                             As official bearers of prestigious American fragrance brand, we offer an exceptionally handpicked selection of authentic products and cosmetics.</p> <br />
                         <p>Libra's Essence is a rapidly growing American perfume brand, inspired by the numerous luxurious American fragrances that delight people all over the world.</p>
                     </div>
-                    <div className="about-page-h2">
+                    <div className="about-us-h2">
                         <h2>Our mission</h2>
                     </div>
                     <div className="about-us-p2">
                         <p>The basis of our philosophy is the pursuit of excellent service and building long-term relationships with customers. It is this attitude that is the basis of the rapid development of our brand. We are proud that we manage to maintain some of the most competitive prices for American perfumes on the worldwide market, without compromising on the quality and vision of the products.</p> <br />
                         <p> Our main goal is for customers to be well served and to return to us again, this is one of the reasons for the rapid growth of the brand. The prices that we strive to maintain are some of the best on the Bulgarian market with guaranteed quality and a great vision of the products.</p>
+                    </div>
+                    <div className="about-us-li-1">
+                        <h2>How can we help?</h2>
+                        <p>Our expert team is at your disposal for:</p>
+                        <ul>
+                            <li>Professional advice on choosing a fragrance.</li>
+                            <li>Information on availability and delivery.</li>
+                            <li>Questions about partnerships and wholesale sales.</li>
+                            <li>Assistance with online orders.</li>
+                        </ul>
+                    </div>
+                    <div className="about-us-li-2">
+                        <h2>Business Opportunities</h2>
+                        <p>We are official importers of original and luxury Arabic brands and we sell both retail and wholesale. The company offers the opportunity for a good partnership as our main goal is for it to be long-term, and this is possible when both parties are satisfied. For questions of any nature, our team is available 24/7.</p>
+                        <p>
+                            We offer excellent conditions for business partnerships, striving for long-term relationships based on mutual benefit. If you are interested in:</p>
+                        <ul>
+                            <li>Wholesale</li>
+                            <li>Distribution</li>
+                            <li>Business Partnership</li>
+                        </ul>
+                        <p>Do not hesitate to contact us for more information about the terms and conditions and opportunities for cooperation.</p>
+                    </div>
+                    <div className="about-us-p3">
+                        <h2>Personal attention</h2>
+                        <p>When you visit our store, you will receive personalized attention and the opportunity to try out different fragrances from our rich collection. Our consultants will help you find the perfect perfume tailored to your preferences.</p>
+                        <p>Whether you are an end customer or a potential business partner, you can count on our team for professional assistance in all matters. Communication with customers is our priority - that's why we are available 24/7 to answer your inquiries.</p>
+                        <p>We are waiting for you!</p>
                     </div>
                 </div>
             </div>
